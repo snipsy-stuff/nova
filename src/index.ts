f7cd6213c2442badc8214a51e14a1bfe4566802b
snipsy-dev (snipsy) <snipsy69@gmail.com>
2025-07-18 20:08:55 +0200

    actually start the bot

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 import { NovaShardClient } from 'packages/client/ShardClient';
 import { parseEnv } from 'packages/util/env';
 
-const client = new NovaShardClient(parseEnv('./.env').DISCORD_TOKEN);
+const client = new NovaShardClient(
+    parseEnv('./.env.local').DISCORD_TOKEN,
+);
 client.on('gatewayReady', () => console.log('ready.'));
+
+async function start() {
+    await client.run({ wait: true });
+}
+
+start().catch(console.error);
